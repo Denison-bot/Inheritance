b25f9a1411038d48820810755236a9a96ed8b1c1
Denison Percey <denisonipercey@outlook.com>
2022-01-24 13:59:32 -0400

    Initial commit

---
+++ Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance");
+            Console.WriteLine();
+
+            Player player = new Player();
+            Enemy enemy = new Enemy();
+
+            player.health = 100;
+            Console.WriteLine("player health: " + player.health);
+            player.TakeDamage(50);
+            Console.WriteLine("player health: " + player.health);
+
+            enemy.health = 50;
+            Console.WriteLine("enemy health: " + enemy.health);
+            enemy.TakeDamage(25);
+            Console.WriteLine("enemy health: " + enemy.health);
+
+            Console.ReadKey(true);
+        }
+    }
+}
